6c46b080718bae36b71cb7ea0aae1dcd9c0955e8
Alex Bender <axbndr@gmail.com>
2017-10-04 19:14:41 +0300

    Do not ask approval if there are no changes

--- roamer/session.py
+++ roamer/session.py
@@ -34,11 +34,16 @@ class Session(object):
         engine = Engine(self.directory, self.edit_directory)
         engine.compile_commands()
         print(engine.commands_to_str())
-        answer = 'y'
-        if not self.skipapproval:
-            answer = input('Please indicate approval: [y/N] ')
-        if not answer or answer[0].lower() != 'y':
-            print('You did not indicate approval')
-            exit(1)
+        if engine.commands:
+            answer = 'y'
+            if not self.skipapproval:
+                print(
+                    'Argument --skip-approval could be used to run roamer '
+                    'in a noninteractive mode.'
+                )
+                answer = input('Please indicate approval: [y/N] ')
+            if not answer or answer[0].lower() != 'y':
+                print('You did not indicate approval.')
+                exit(1)
         engine.run_commands()
         record.add_dir(Directory(self.cwd, os.listdir(self.cwd)))
